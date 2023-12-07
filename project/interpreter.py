0af59f96613b1d23489e5e817307fadd59cc8b23
yanyi0222 <jeremylai960222@gmail.com>
2023-12-07 11:27:07 +0800

    20231207

--- project/interpreter.py
+++ project/interpreter.py
@@ -1,51 +1,74 @@
+import pickle
+
 import cv2
 import mediapipe as mp
+import numpy as np
 
-# 初始化MediaPipe Hand模塊
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
 
-# 初始化MediaPipe繪圖模塊
-mp_drawing = mp.solutions.drawing_utils
-
-# 創建一個VideoCapture對象，指定相機索引，通常0表示默認相機
 cap = cv2.VideoCapture(0)
 
-# 檢查攝像頭是否成功打開
-if not cap.isOpened():
-    print("無法打開攝像頭。請確保相機可用並已連接。")
-    exit()
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels_dict = {0: '1', 1: '2', 2: '3'}
 while True:
-    # 讀取一個視頻幀
-    ret, frame = cap.read()
-    
-    if not ret:
-        print("無法讀取視頻幀。")
-        break
 
-    # 將視頻幀轉換為RGB格式
+    data_aux = []
+    x_ = []
+    y_ = []
+
+    ret, frame = cap.read()
+
+    H, W, _ = frame.shape
+
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # 使用MediaPipe進行手部檢測
     results = hands.process(frame_rgb)
-
     if results.multi_hand_landmarks:
-        for landmarks in results.multi_hand_landmarks:
-            # 在這裡實現手語翻譯的邏輯
-            # 可以根據手部地標和連接線的位置來識別特定的手勢
-            # 並提供相應的翻譯或操作
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,  # image to draw
+                hand_landmarks,  # model output
+                mp_hands.HAND_CONNECTIONS,  # hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
 
-            # 繪製手部地標
-            mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
 
-    # 在視窗中顯示當前視頻幀
-    cv2.imshow("Sign Language Interpreter", frame)
+                x_.append(x)
+                y_.append(y)
 
-    # 檢查是否按下 'q' 鍵，如果是，則退出循環
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
 
-# 釋放攝像頭資源並關閉視窗
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
+
+        prediction = model.predict([np.asarray(data_aux)])
+
+        predicted_character = labels_dict[int(prediction[0])]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+
 cap.release()
 cv2.destroyAllWindows()
